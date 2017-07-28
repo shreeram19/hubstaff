ac978307c02a21d4d7b153213a8142844b4c0db3
SHREERAM PARTHASARATHY <shreeramparthasarathy@SHREERAMs-MacBook-Pro.local>
2017-07-28 23:21:05 +0530

    Intial Set-up

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt){
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    
+    grunt.registerTask('compilestyle',['sass'],function(){
+        console.log("Compiling the SCSS to CSS");
+        grunt.task.run('sass');
+        console.log("Done");
+    });
+    
+    grunt.initConfig({
+        copy:{
+            dist :{
+                files:[{
+                    expand: true,
+                    cwd: 'app/',
+                    src: '**/*.html',
+                    dest:'build/'
+                },
+                {
+                    expand: true,
+                    cwd: 'app/',
+                    src: '**/*.js',
+                    dest:'build/'
+                },
+            ]
+            }
+        },
+        clean:['app/all*','build'],
+        sass:{
+            dist:{
+               files:[{
+                   src:['app/styles/*'],
+                   dest:'build/all.css'
+               }] 
+            }
+        },
+        watch:{
+            css:{
+                files:['app/styles/*','app/**/*.js','app/**/*.html'],
+                tasks:['compilestyle','copy']
+            }
+        }
+    });
+    
+    
+    grunt.registerTask('default',['clean','compilestyle','copy','watch']);
+
+};
